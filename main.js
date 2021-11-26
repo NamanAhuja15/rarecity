70da001a65a353e1a022695cc5e826b64bd4032e
NamanAhuja15 <46511996+NamanAhuja15@users.noreply.github.com>
2021-11-27 05:21:38 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -211,7 +211,7 @@ async function assetunstake(assetId) {
     try {
 
       var data1 = {
-        asset_id: [assetId]
+        asset_ids: [assetId]
       };
       const result = await wallet_transact([{
         account: contract,
